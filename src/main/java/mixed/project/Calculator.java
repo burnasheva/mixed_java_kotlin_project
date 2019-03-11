0642c8118bf6df5f828e1d7b7a557ca39313ba78
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2019-03-11 12:45:52 +0100

    remove unused files

--- src/main/java/mixed/project/Calculator.java
+++
@@ -1,9 +0,0 @@
-package mixed.project;
-
-public class Calculator {
-
-    public static int plus(int a, int b){
-        return a + b;
-    }
-
-}
--- src/main/kotlin/mixed/project/KotlinCalculator.kt
+++
@@ -1,6 +0,0 @@
-package mixed.project
-
-object KotlinCalculator {
-
-    fun plus(a: Int, b: Int) : Int = a + b
-}
