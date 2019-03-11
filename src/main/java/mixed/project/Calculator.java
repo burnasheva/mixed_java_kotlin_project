282ed8cd15ff3941d10295da0ce78e8de5a3d637
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2019-03-11 09:16:34 +0100

    add simple mixed project

---
+++ src/main/java/mixed/project/Calculator.java
@@ -0,0 +1,9 @@
+package mixed.project;
+
+public class Calculator {
+
+    public static int plus(int a, int b){
+        return a + b;
+    }
+
+}
---
+++ src/main/kotlin/mixed/project/KotlinCalculator.kt
@@ -0,0 +1,6 @@
+package mixed.project
+
+object KotlinCalculator {
+
+    fun plus(a: Int, b: Int) : Int = a + b
+}
