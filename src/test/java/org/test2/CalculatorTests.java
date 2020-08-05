61522a06949b056184d3fbd6270ed4e903c7ab36
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2020-08-05 15:17:21 +0200

    add kotlin.collections import

--- src/test/java/org/test2/CalculatorTests.java
+++ src/test/java/org/test2/CalculatorTests.java
@@ -1,5 +1,6 @@
 package org.test2;
 
+import kotlin.collections.AbstractList;
 import org.junit.Assert;
 import org.junit.Test;
 import org.test1.KotlinCalculator;
@@ -7,11 +8,13 @@ import org.test1.KotlinCalculator;
 public class CalculatorTests {
     @Test
     public void testPlus(){
+        kotlin.collections.AbstractList<KotlinCalculator> some = testKotlinCalculator();
         Assert.assertEquals(5, JavaCalculator.plus(3, 2));
     }
 
     @Test
-    public void testKotlinCalculator(){
+    public AbstractList<KotlinCalculator> testKotlinCalculator(){
         Assert.assertEquals(5, KotlinCalculator.INSTANCE.plus(3, 2));
+        return null;
     }
 }
