e1cda0956d83b0a3e0fb7a2bd43825be6fe07113
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2020-08-06 10:09:58 +0200

    fix test

--- src/test/java/org/test2/CalculatorTests.java
+++ src/test/java/org/test2/CalculatorTests.java
@@ -1,6 +1,5 @@
 package org.test2;
 
-import kotlin.collections.AbstractList;
 import org.junit.Assert;
 import org.junit.Test;
 import org.test1.KotlinCalculator;
@@ -8,13 +7,11 @@ import org.test1.KotlinCalculator;
 public class CalculatorTests {
     @Test
     public void testPlus(){
-        kotlin.collections.AbstractList<KotlinCalculator> some = testKotlinCalculator();
         Assert.assertEquals(5, JavaCalculator.plus(3, 2));
     }
 
     @Test
-    public AbstractList<KotlinCalculator> testKotlinCalculator(){
+    public void testKotlinCalculator(){
         Assert.assertEquals(5, KotlinCalculator.INSTANCE.plus(3, 2));
-        return null;
     }
 }
